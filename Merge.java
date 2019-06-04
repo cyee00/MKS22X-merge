c94c33a3c5c790824b741a2440e31dcaa079e148
cyee00 <cyee00@stuy.edu>
2019-06-04 11:00:14 -0400

    init/working on helper methods

---
+++ Merge.java
@@ -0,0 +1,23 @@
+public class Merge{
+  public static void mergesort(int[]data){
+
+  }
+  private static int[] sort(int[]data){
+    int half;
+    int[] a,b;
+    if (data.length%2==0){
+      half=data.length/2;
+    } else {
+      half = data.length/2+1;
+    }
+    int[] a = new int[half];
+    int[] b = new int[data.length-half]
+    for (int i=0;i<half,i++){
+
+    }
+    return merge(a,b);
+  }
+  private static int[] merge(int[]a,int[]b){
+    return a;
+  }
+}
