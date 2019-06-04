700ef4c8a03c5a19b31481f2dde5625efe2e7885
cyee00 <cyee00@stuy.edu>
2019-06-04 11:24:37 -0400

    working on merge and sort

--- Merge.java
+++ Merge.java
@@ -4,6 +4,23 @@ public class Merge{
   }
 
   private static int[] sort(int[]data){
+  if (a.length==1&&b.length==0){
+    return a;
+  }
+  if (b.length==1&&a.length==0){
+    return b;
+  }
+  if (a.length==1&&b.length==1){
+    if (a[0]<b[0]){
+      ans[0]=a[0];
+      ans[1]=b[0];
+      return ans;
+    } else {
+      ans[0]=b[0];
+      ans[1]=a[0];
+      return ans;
+    }
+  }
     int half;
     if (data.length%2==0){
       half=data.length/2;
@@ -18,33 +35,25 @@ public class Merge{
     for (int i=half;i<data.length;i++){
       b[i-half]=data[i];
     }
-    return merge(a,b);
+    return merge(sort(a),sort(b));
   }
 
   private static int[] merge(int[]a,int[]b){
     int[] ans = new int[a.length+b.length];
-    if (a.length==1&&b.length==0){
-      return a;
-    }
-    if (b.length==1&&a.length==0){
-      return b;
-    }
-    if (a.length==1&&b.length==1){
-      if (a[0]<b[0]){
-        ans[0]=a[0];
-        ans[1]=b[0];
-        return ans;
-      } else {
-        ans[0]=b[0];
-        ans[1]=a[0];
-        return ans;
-      }
-    }
     int counterA = 0;
     int counterB = 0;
+    int index = 0;
     while (counterA<a.length&&counterB<b.length){
-      if (a[counterA]<b[counterB])
+      if (b[counterB]<a[counterA]){
+        ans[index]=b[counterB];
+        counterB++;
+      } else {
+        ans[index]=a[counterA];
+        counterA++;
+      }
+      index++;
     }
+    return ans;
   }
 
 }
